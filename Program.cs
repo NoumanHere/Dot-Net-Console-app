f792e67ad102ba48ba9575859b4ebf50d62442a9
NoumanHere <noumannow@gmail.com>
2022-07-28 11:03:38 +0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dot_Net_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
